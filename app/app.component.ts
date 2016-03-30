9a1f78dd244a467fd0752ebf71578c36d23df664
zc1415926 <zc1415926@126.com>
2016-03-30 14:23:48 +0800

    5 MIN QUICKSTART
    Finish the tutorial at:
    https://angular.io/docs/ts/latest/quickstart.html

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by zc1415926 on 2016/3/30.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App!</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by zc1415926 on 2016/3/30.
+ */
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
