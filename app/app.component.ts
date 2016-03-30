29740c6d041c757aad028d4e9fb9c8d045761dc7
zc1415926 <zc1415926@126.com>
2016-03-30 15:08:40 +0800

    THE HERO EDITOR
    Finish the tutorial:
    https://angular.io/docs/ts/latest/tutorial/toh-pt1.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,23 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App!</h1>'
+    template:
+    '<h1>{{title}}</h1>' +
+    '<h2>{{hero.name}} details!</h2>' +
+    '<div><label>id: </label>{{hero.id}}</div>' +
+    '<div><label>name: </label>' +
+    '<input [(ngModel)]="hero.name" placeholder="name"></div>'
 })
-export class AppComponent { }
+export class AppComponent {
+    public title = 'Tour of Heroes';
+
+    public hero: Hero = {
+        id: 1,
+        name: 'windstorm'
+    };
+}
+
+export class Hero{
+    id: number;
+    name: string;
+}
